9da9843ef3fef45b7c7701328bb01e8077928fc5
alvaroluna <luna.alvaro3@gmail.com>
2020-05-30 16:38:31 -0500

    changed model file name string extension

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // index.js exports the schemas for use in SOMETHING
 
 module.exports = {
-    Headline: require("./Headline"),
-    Note: require("./Note")
+    Headline: require("./Headline.js"),
+    Note: require("./Note.js")
 };
